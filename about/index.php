08790bb93a061f47cd8cfe08b1b8b222c6fca7bf
Linh <linhvhnguyen@gmail.com>
2018-02-24 18:18:44 -0600

    Create About Page

--- about/index.php
+++ about/index.php
@@ -14,8 +14,8 @@
     <!-- BANNER SECTION -->
      <div class="banner row">
          <div class="image large-12 large-centered columns">
-             <img src="../img/about.png">
-             <h2>ABOUT THE WSU COMPUTER SCIENCE DEPARTMENT</h2>
+             <img src="../img/3women-24.png">
+             <h2>ABOUT WiCS</h2>
          </div>
      </div>
      <!-- DIVIDER -->
@@ -25,75 +25,53 @@
       <div class="large-12 columns">
         <div>
             <section>
-                <p>Working with such rapidly changing technology in today’s world requires an increasing level of skill and education. In addition to technical knowledge, companies want professionals who have communication and other interpersonal skills and can adapt their problem-solving skills to different situations. The Computer Science program at Winona State is designed to prepare students to the fullest extent. Our students take courses to develop not only their programming skills and technical knowledge, but also their communication and critical thinking skills.</p>
+                <p> We are a supportive network to help women join and enjoy the culture of computer science and its project. Every week, we meet to help, inspire, and challenge each other. Or just to chat and make new friends because it's important to have someone who understands the challenges women in computer science faces everyday.</p>
                 
                 <div class="row"><img src="../img/line.svg"></div>
                 
-                <h2 class="teal">Class Size</h2>
-                    <p>
-                       Class sizes are kept to a minimum (25-35 students) with emphasis on instruction and active learning process. 
-                    </p>
-                <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="yellow">Faculty</h2>
-                    <p>The Computer Science department provides enthusiastic faculty who enjoy working with and  advising students. They teach our hands-on, lab-based curriculum. Personal attention and quality of teaching are strong points of Winona State's Computer Science Department.
-                    </p>
-                    <br>
-                     <a href=<?php print($path . "faculty/") ?> class="medium button button-yellow">Meet the Faculty <i class="fa fa-chevron-right"></i></a>
-                    <br>
-                <div class="row major"><img src="../img/line.svg"></div>
-                <h2 class="orange">The Computer Science Major</h2>
-                    <p>Are you looking for a fun, challenging degree program that will lead to an interesting job that pays well? Then you've come to the right place!
-
-
-                    According to the US Government Bureau of Labor Statistics: </p>
                 
-                <blockquote class="block">"Overall, employment of computer software engineers and computer programmers is projected to increase much faster than the average for all occupations. Job prospects should be best for those with a bachelor's degree and relevant experience."</blockquote> <p>(2015)</p>
-               
-                
-                <p>According to the national ACM, there are lots of good reasons to major in Computer Science. Here are just a few:</p>
+                 <h2 class="teal">Missions</h2>
                 <br>
                 <ul>
-                    <li>Computing is part of everything we do</li>
-                    <li>Computing offers great opportunities for true creativity and innovativeness</li>
-                    <li>Computing enables you to make a positive difference in the world</li>
-                    <li>Computing jobs are here to stay, regardless of where you are located</li>
-                    <li>Computing offers many types of lucrative careers </li>
+                    <li>Increase the participation of women in the field of computer science</li>
+                    <li>Build and strengthen the community of women in computer science</li>
+                    <li>Raise awareness of the issues women in the industry faces</li>
+                    <li>Promote and support the women in Computer Science community</li>
+                    <li>Encourage women in their educational and professional development</li>
                 </ul>
                 
-                <br>
-                <a href="http://www.payscale.com/college-salary-report/best-schools-by-majors/computer-science" target="_blank" class="medium button button-orange">How well does it pay to be a CS major? <i class="fa fa-chevron-right"></i></a>
-                <br>
+                
+                <div class="row"><img src="../img/line.svg"></div>
+                
+                
+                <h2 class="orange">Founders</h2>
+                    <p>Women in Computer Science (WiCS) was formed by female undergraduate students of Winona State University. Jimmy also helped.
+                    </p>
+                    <br>
+                     <a href=<?php print($path . "Members/") ?> class="medium button button-orange">Meet the Members<i class="fa fa-chevron-right"></i></a>
+                    <br>
+    
+             
                 
                <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="lime">Student Opportunities</h2>
-                    <p>The Computer Science Department at Winona State offers many opportunities for students to expand learning beyond traditional courses, including:</p>
+                <h2 class="lime">What we do</h2>
+                    <p>WiCS offers many opportunities for our members including:</p>
                     <br>
                     <ul>
-                        <li>Specialized Scholarships</li>
-                        <li>Senior Research Projects</li>
-                        <li>Practicum (Internships)</li>
-                        <li>Experience in the Field through the Software Testing and Development Lab</li>
-                        <li>Tutoring</li>
-                        <li>Computer Science Club</li>
+                        <li>Weekly meeting</li>
+                        <li>Projects</li>
+                        <li>Mentoring</li>
+                        <li>Hackathons</li>
+                        <li>VR games - HTC Vive</li>
+                        <li>3D printing</li>
+                        <li>Webpage development (This entire webpage was developed by our members!) </li>
                     </ul>
-                    <p>The department prepares students for long-term success by combining these opportunities.</p>
                     <br>
-                    <a href="<?php print($path . "opportunities/") ?>" class="button button-lime radius">Beyond the Classroom <i class="fa fa-chevron-right"></i></a>
+                    <a href="<?php print($path . "opportunities/") ?>" class="button button-lime radius">Event page <i class="fa fa-chevron-right"></i></a>
                     <br>
                 <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="teal">Future Careers</h2>
-                    <p>The opportunities are almost endless when you are looking for a career in the computer science field. Because the WSU computer science department exposes its students to many different concepts, the amount of job opportunities is limitless. Some potential jobs you could obtain could be:</p>
-                    <br>
-                    <ul>
-                        <li>Computer Programmer</li>
-                        <li>IT Support Specialist</li>
-                        <li>Software Application Developer</li>
-                        <li>Database Administrator</li>
-                        <li>Web Developer</li>
-                        <li>Software Quality Assurance Tester </li>
-                    </ul>
-                <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="yellow">CS Program</h2>
+                
+                <h2 class="yellow">WSU Computer Science Program</h2>
                     <p>Computer science majors can choose from two options: Computer Science and Applied Computer Science. Both options require a common core set of courses designed to give students a fundamental understanding of both theoretical and applied computer science.
 
                         In addition to the programs for the Computer Science Major, the department offers a Computer Science Minor and a Computer Technology Minor.
@@ -104,35 +82,18 @@
                 <br>
                 
                 <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="orange">Laptop Program</h2>
-                <p>Winona State University offers the e-Warrior program to students which equips them with a laptop and technical support to give them access to the latest technology. This program helps students with everything from printing on campus to downloading the latest software suites from Adobe and Microsoft.
-                </p>
-                
-                <br>
-                <a href="http://www.winona.edu/technology/tech-support.asp" target="_blank" class="button button-orange radius">More About Our Technology <i class="fa fa-chevron-right"></i></a>
+                <a href="#"><h2 class="teal">Questions?</h2></a>
                 <br>
                 
-                <div class="row"><img src="../img/line.svg"></div>
-                <h2 class="lime">History of the department</h2>
-                    <p>The Winona State University Computer Science Department was formed in 1987. The department has taught its core classes in many different languages over the years. Check out the history page to learn more about this history of the WSU Computer Science Department.
-                    </p>
-                    <br>
-                     <a href="<?php print($path . "history/") ?>" class="medium button button-lime">More history <i class="fa fa-chevron-right"></i></a>
-                    <br>
-                
-                <div class="row"><img src="../img/line.svg"></div>
-                <a href="#"><h2 class="teal">Rochester Program</h2></a>
-                    <p>Our program at Winona State University (WSU) operates on two campuses, one in Winona (main campus) and one in Rochester (WSU-R). Although we have different personnel and student demographics on both campuses, we function as one department in terms of policies, programming, and governance.
-                </p>
-                <br>
-                
-                <a href="http://www.winona.edu/rochester/" target="_blank" class="medium button button-teal">More About Rochester Program <i class="fa fa-chevron-right"></i></a>
+                <a href="http://www.winona.edu/rochester/" target="_blank" class="medium button button-teal">Contact us <i class="fa fa-chevron-right"></i></a>
                 <br>
             </section>
           </div>
         </div>
     </div>
         <br>
+        
+        <h5>Made By: Linh Nguyen</h2>
     <!-- DIVIDER -->
     
     
@@ -156,3 +117,4 @@
       </div>
   </body>
 </html>
+
