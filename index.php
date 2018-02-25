53ea1410c37c5f99114196d06e7fe417a902ae3d
nhorabik15 <nhorabik15@winona.edu>
2018-02-24 18:00:41 -0600

    Created FAQ Page

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
             
               <a target="_blank" href="http://www.winona.edu/"><span class="bottom-head">WINONA STATE UNIVERSITY'S</span></a>
 
-            <a href=<?php print($path) ?>><h2 class="cs-title">COMPUTER SCIENCE<br>DEPARTMENT</h2></a>
+            <a href=<?php print($path) ?>><h2 class="cs-title">COMPUTER SCIENCE<br>Women in Computer Science</h2></a>
             <br>
             <a href="donate/" class="medium button button-yellow special-button">DONATE</a>
               <a href="http://www.winona.edu/admissions/apply.asp" class="medium button button-lime special-button" target="_blank"><span class="apply-button">APPLY NOW</span></a>
@@ -34,57 +34,46 @@
 <div class="skewed-bg-green"></div>
      
      
-         <!-- NEWS CONTENT -->
-         <div class="row show-for-medium">
-         <h2>News</h2>
-         </div>
-         
-    
-    <!-- | SECTION |-->
-    <div class="row show-for-medium " >
-        <?php include 'news/newsplugin.php'; ?>
-
-      <div class="row">
-        <div class="medium-12 columns news">
-              <a href="./news" class="button button-orange radius">Read More News <i class="fa fa-chevron-right"></i></a>
-        </div>
-      </div>
-    </div>
-    
-    <!-- DIVIDER -->
-     <div class="row show-for-medium"><img src="img/line.svg "></div>
-     
-     <br>
-     <br>
- 
  <!-- Added Image class -->
      <div class="row firstSection">
         <div class="columns">
+            
+            
             <div class="image large-4 medium-4 small-12 columns">
                 <img src="img/About.png" alt="">
-                <h3>About</h3>
-                <p>The CS Program includes programming and technical knowledge, critical thinking skills, and fun.</p>
-                <a href="<?php print($path . "about/") ?>" class="medium button button-lime">Read More <i class="fa fa-chevron-right"></i></a>
+                <h3>Mission Statement</h3>
+                <p>Click here to view WiCS Mission Statement.</p>
+                <a href="<?php print($path . "about/") ?>" class="medium button button-lime">Mission Statement <i class="fa fa-chevron-right"></i></a>
             </div>
+            
+            
             <div class="image large-4 medium-4 small-12 columns">
                 <img src="img/prospective-students.png" alt="">
-                <h3>Beyond the Classroom</h3>
-                <p>Beyond classes, student opportunities include practicum, scholarships, research, and more.</p>
-                <a href="<?php print($path . "opportunities/") ?>" class="medium button button-yellow">Read More <i class="fa fa-chevron-right"></i></a>
+                <h3>Scholarships</h3>
+                <p>Link to many scholarships specific to CS and women in CS.</p>
+                <a href="<?php print($path . "opportunities/") ?>" class="medium button button-yellow">Scholarships <i class="fa fa-chevron-right"></i></a>
             </div>
+            
+            
             <div class="image large-4 medium-4 small-12 columns">
                 <img src="img/faculty.png" alt="">
-                <h3>Faculty</h3>
-                <p>Our courses are taught by professors who are experienced and passionate. Come learn more from experts in their field.</p>
-                <a href=<?php print($path . "faculty/") ?> class="medium button button-orange">Read More <i class="fa fa-chevron-right"></i></a>
+                <h3>Email List</h3>
+                <p>Type your email to be added to our email list!</p>
+                <a href=<?php print($path . "faculty/") ?> class="medium button button-orange">Email List <i class="fa fa-chevron-right"></i></a>
             </div>
+            
+            
             <div class="medium-4">
             </div>
         </div>
     </div> 
+
+
     <!-- DIVIDER -->
      <div class="row"><img src="img/line.svg"></div>
      
+
+
      <!-- SECTION -->
      <div class="row">
          <div class="image small-12 medium-4 medium-push-8 large-6 large-push-6 columns">
